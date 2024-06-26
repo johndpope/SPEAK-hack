2bef2ff54e18d69d3162804c7a947247ae3e7266
John Pope <jp@bellgeorge.com>
2024-06-26 21:48:14 +1000

    ok

--- train.py
+++ train.py
@@ -122,8 +122,9 @@ def train_loop(config, model, dataloader, optimizer):
         progress_bar.set_description(f"Epoch {epoch}")
 
         for step, batch in enumerate(dataloader):
-            images = batch["img_pairs"].to(accelerator.device)
-            x_s, x_t = images[:, 0], images[:, 1]  # Assuming the batch contains pairs of images
+            x_s = batch["source_image"].to(accelerator.device)
+            x_t = batch["target_image"].to(accelerator.device)
+            
             print("x_s:",x_s.shape)
             print("x_t:",x_t.shape)
             
@@ -181,8 +182,8 @@ def main():
         for i in range(0, len(examples["image"]), 2):
             source_image = preprocess(examples["image"][i].convert("RGB"))
             target_image = preprocess(examples["image"][i + 1].convert("RGB"))
-            image_pairs.append((source_image, target_image))
-        return {"img_pairs": torch.stack(image_pairs)}
+
+        return {"source":source_image,"target_image":target_image}
 
     dataset.set_transform(transform)
     dataloader = DataLoader(dataset, batch_size=config.training.train_batch_size, shuffle=True)
