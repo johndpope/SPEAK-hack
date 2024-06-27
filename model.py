61a06f8bb4e2c67b700cfdfda3f8aecd884f64f6
John Pope <jp@bellgeorge.com>
2024-06-27 20:40:38 +1000

    ok

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ class IRFDGenerator512(nn.Module):
     def __init__(self, input_dim, ngf=64):
         super(IRFDGenerator512, self).__init__()
         
-          self.main = nn.Sequential(
+        self.main = nn.Sequential(
             # Input is the concatenated identity, emotion and pose embeddings
             # input_dim = 3 * 2048 = 6144 (assuming ResNet-50 encoders)
             nn.ConvTranspose2d(input_dim, ngf * 64, 4, 1, 0, bias=False),
