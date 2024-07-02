341a3cef03fe112d7e68c545d26da0250db70aea
John Pope <jp@bellgeorge.com>
2024-07-03 08:12:11 +1000

    ok

--- CelebADataset.py
+++ CelebADataset.py
@@ -53,8 +53,10 @@ class AffectNetDataset(Dataset):
         self.feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
 
 
-        # Define emotion class to index mapping
-        self.emotion_class_to_idx = {str(i): i for i in range(8)}
+        self.emotion_class_to_idx = {
+            'angry': 0, 'contempt': 1, 'disgust': 2, 'fear': 3, 'happy': 4, 
+            'neutral': 5, 'sad': 6, 'surprise': 7
+        }
 
         # Load image paths and their corresponding emotion labels
         self.image_paths = []
