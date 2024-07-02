c02cbf17d5e1dcf34daa6889510b1db9f74f33c3
John Pope <jp@bellgeorge.com>
2024-07-03 08:19:23 +1000

    ok

--- CelebADataset.py
+++ CelebADataset.py
@@ -54,8 +54,8 @@ class AffectNetDataset(Dataset):
 
 
         self.emotion_class_to_idx = {
-            'angry': 0, 'contempt': 1, 'disgust': 2, 'fear': 3, 'happy': 4, 
-            'neutral': 5, 'sad': 6, 'surprise': 7,'happiness':4
+            'anger': 0, 'contempt': 1, 'disgust': 2, 'fear': 3, 'happiness': 4, 
+            'neutral': 5, 'sadness': 6, 'surprise': 7
         }
 
         # Load image paths and their corresponding emotion labels
